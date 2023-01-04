316b5cebe344fac1dff88e408bad33ce04475c37
Avi241 <arvind444pandit@gmail.com>
2023-01-04 12:58:12 +0530

    first commit

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/arvind/haptic_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/arvind/mas_ws/devel;/home/arvind/beta_ws/devel;/home/arvind/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/arvind/mas_ws/devel;/home/arvind/beta_ws/devel;/home/arvind/catkin_ws/devel;/opt/ros/noetic' != '' else []
